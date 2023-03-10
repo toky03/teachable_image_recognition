b6ec8a17e6f3c0ea5ad9bd812bf9a53343bea6c1
Marco <marco.jakob3@gmail.com>
2023-03-10 08:10:22 +0100

    make base folder for collecting photos a variable

--- photo_collector/main.py
+++ photo_collector/main.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 
+base_folder = ''
+
 
 def create_folder_if_not_exists(path_name):
     if not os.path.exists(path_name):
@@ -8,24 +10,28 @@ def create_folder_if_not_exists(path_name):
 
 
 def prepare_folders():
-    create_folder_if_not_exists('image_set')
-    create_folder_if_not_exists('image_set/label_a')
-    create_folder_if_not_exists('image_set/label_b')
+    create_folder_if_not_exists(base_folder+'/image_set')
+    create_folder_if_not_exists(base_folder+'/image_set/label_a')
+    create_folder_if_not_exists(base_folder+'/image_set/label_b')
 
 
 def collect_pictures(video):
     counter = 0
     while True:
         ret, frame = video.read()
+        frame = cv2.rotate(frame,cv2.ROTATE_180)
+        frame = cv2.resize(frame, (160, 160))
         cv2.imshow('frame', frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
         elif key == ord('a'):
-            cv2.imwrite('image_set/label_a/{}.png'.format(str(counter)), frame)
+            cv2.imwrite(base_folder+'/image_set/label_a/{}.png'.format(str(counter)), frame)
+            print('image a saved')
             counter += 1
         elif key == ord('b'):
-            cv2.imwrite('image_set/label_b/{}.png'.format(str(counter)), frame)
+            cv2.imwrite(base_folder+'/image_set/label_b/{}.png'.format(str(counter)), frame)
+            print('image b saved')
             counter += 1
 
 
